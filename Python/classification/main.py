05ce89a5ec3b5c594f362a7523b168028f335f7c
Daniel Nicolau <2232623@my.ipleiria.pt>
2023-12-11 19:52:52 +0000

    Create main.py

---
+++ Python/classification/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+
+
+
+
+if __name__ == "__main__":
+    data = pd.read_excel("data/Datasets_Group_B.xlsx", "Classification")
+    train_data = data[]
+    vali_data = 
+    test_data = 
+    labels = data["Motion Detection"].to_numpy()
