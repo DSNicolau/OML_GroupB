de257d8d51cc9b91d9a316ee59880e2e1a348dc6
Daniel Nicolau <2232623@my.ipleiria.pt>
2023-12-11 19:54:58 +0000

    Update main.py

--- Python/classification/main.py
+++ Python/classification/main.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 if __name__ == "__main__":
     data = pd.read_excel("data/Datasets_Group_B.xlsx", "Classification")
-    train_data = data[]
-    vali_data = 
-    test_data = 
+    train_data = data.iloc[:int(len(data)*0.64)]
+    vali_data = data.iloc[int(len(data)*0.64):int(len(data)*0.8)]
+    test_data = data.iloc[int(len(data)*0.8):]
     labels = data["Motion Detection"].to_numpy()
