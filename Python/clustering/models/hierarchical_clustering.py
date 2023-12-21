adedb2dcdee6e236b1f33cba0da9d9d8fd50e51a
Daniel Nicolau <2232623@my.ipleiria.pt>
2023-12-21 17:38:40 +0000

    ah

--- Python/clustering/models/hierarchical_clustering.py
+++ Python/clustering/models/hierarchical_clustering.py
@@ -10,4 +10,8 @@ class HierarchicalClustering(Model):
 
     def predict(self, linkage="ward", metric="euclidean", n_clusters=2):
         
+        # Initialise the clusters
+        clusters = [[point] for point in self.data]
+
+        while len(clusters) > n_clusters
     
